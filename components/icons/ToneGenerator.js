b83b397e3a8a413aef0c76caf664c75bc4d9779c
Kürşad Şimşek <80485805+kursadjs@users.noreply.github.com>
2023-01-20 19:16:06 +0300

    add icons

---
+++ components/icons/ToneGenerator.js
@@ -0,0 +1,25 @@
+import * as React from 'react'
+const SvgToneGenerator = (props) => (
+  <svg
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M21.97 12c0 2.94-1.28 5.6-3.32 7.41A9.836 9.836 0 0 1 12 21.97c-5.5 0-9.97-4.47-9.97-9.97 0-5.5 4.47-9.97 9.97-9.97 2.56 0 4.89.96 6.65 2.56A9.89 9.89 0 0 1 21.97 12Z"
+      stroke="currentColor"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M11.97 16.39c-.02 1.65-1.25 2.05-2.4 1.59A6.439 6.439 0 0 1 5.53 12c0-2.71 1.67-5.03 4.04-5.99 1.15-.46 2.38-.05 2.4 1.59v8.79Z"
+      stroke="currentColor"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+export default SvgToneGenerator
